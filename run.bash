6c4a20a028c7e888131d66a609e5d49fb91a7ee8
ksanchez07 <53946022+ksanchez07@users.noreply.github.com>
2024-10-03 21:25:19 -0500

    Delete run.bash

--- run.bash
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-set -e
-echo "launching server..."
-node server.js &
-echo "launching client..."
-open http://127.0.0.1:8088/client.html &
-echo "waiting for all processes to finish..."
-wait
-echo "Done."
